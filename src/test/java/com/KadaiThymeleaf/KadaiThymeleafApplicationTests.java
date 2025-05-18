5f0246fa51bc070e469de5659514892e56034bea
mana.urano <manaurano@gmail.com>
2025-05-18 12:19:36 +0900

    KadaiThymeleaf

---
+++ src/test/java/com/KadaiThymeleaf/KadaiThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KadaiThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadaiThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
